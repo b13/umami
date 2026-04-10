205e7c44e66f3c581f6e805e3558de89f71601ef
Michael Semle <michael.semle@b13.com>
2026-04-10 16:13:23 +0200

    [TASK] Move umami module to insight parent

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,12 +1,18 @@
 <?php
 
 return [
-    'web_umami' => [
-        'parent' => 'web',
+    'insights' => [
+        'labels' => 'LLL:EXT:umami/Resources/Private/Language/locallang_mod_insights.xlf',
+        'iconIdentifier' => 'module-insights',
+        'extensionName' => 'umami',
+        'position' => ['after' => 'web'],
+    ],
+    'insights_umami' => [
+        'parent' => 'insights',
         'access' => 'user',
         'workspaces' => 'live',
         'path' => '/module/umami',
-        'labels' => 'LLL:EXT:umami/Resources/Private/Language/locallang_mod.xlf',
+        'labels' => 'LLL:EXT:umami/Resources/Private/Language/locallang_mod_umami.xlf',
         'extensionName' => 'Umami',
         'icon' => 'EXT:umami/Resources/Public/Icons/module_info.svg',
         'inheritNavigationComponentFromMainModule' => false,
